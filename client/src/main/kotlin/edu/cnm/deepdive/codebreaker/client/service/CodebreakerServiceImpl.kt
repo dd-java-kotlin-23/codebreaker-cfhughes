613664326521d2897e6c7cd47fc8d8e9f4b4b17b
nick-bennett <nbennett10@cnm.edu>
2026-06-11 15:55:26 -0600

    Successful round-trip on startGame.

--- client/src/main/kotlin/edu/cnm/deepdive/codebreaker/client/service/CodebreakerServiceImpl.kt
+++ client/src/main/kotlin/edu/cnm/deepdive/codebreaker/client/service/CodebreakerServiceImpl.kt
@@ -1,62 +1,139 @@
 package edu.cnm.deepdive.codebreaker.client.service
 
+import com.squareup.moshi.FromJson
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.ToJson
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import edu.cnm.deepdive.codebreaker.client.dto.ErrorResponse
 import edu.cnm.deepdive.codebreaker.client.dto.GameRequest
 import edu.cnm.deepdive.codebreaker.client.dto.GameResponse
 import edu.cnm.deepdive.codebreaker.client.dto.GuessRequest
 import edu.cnm.deepdive.codebreaker.client.dto.GuessResponse
 import edu.cnm.deepdive.codebreaker.client.web.CodebreakerApi
-import okhttp3.OkHttpClient
-import java.util.*
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Properties
 import java.util.concurrent.CompletableFuture
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.future.future
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
-class CodebreakerServiceImpl : CodebreakerService {
+internal object CodebreakerServiceImpl : CodebreakerService {
     private val client: OkHttpClient
+    private val moshi: Moshi
     private val api: CodebreakerApi
+    private val scope: CoroutineScope
 
     init {
-        TODO("Not yet implemented; implement to initialize client and api")
         val properties = loadProperties()
+        moshi = buildMoshi()
+        client = buildClient(properties)
+        api = buildApi(properties, moshi, client)
+        scope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
     }
 
-    override fun startGame(game: GameRequest): CompletableFuture<GameResponse> {
-        TODO("Not yet implemented")
-    }
+    override fun startGame(game: GameRequest): CompletableFuture<GameResponse> =
+        scope.future { handleResponse(api.startGame(game)) }
 
-    override fun getGame(gameId: String): CompletableFuture<GameResponse> {
-        TODO("Not yet implemented")
-    }
+    override fun getGame(gameId: String): CompletableFuture<GameResponse> =
+        scope.future { handleResponse(api.getGame(gameId)) }
 
-    override fun deleteGame(gameId: String): CompletableFuture<Void?> {
-        TODO("Not yet implemented")
-    }
+    override fun deleteGame(gameId: String): CompletableFuture<Void?> =
+        scope.future { handleResponse(api.deleteGame(gameId)) }
 
     override fun submitGuess(
         gameId: String,
         guess: GuessRequest
-    ): CompletableFuture<GuessResponse> {
-        TODO("Not yet implemented")
-    }
+    ): CompletableFuture<GuessResponse> =
+        scope.future { handleResponse(api.submitGuess(gameId, guess)) }
 
     override fun getGuess(
         gameId: String,
         guessId: String
-    ): CompletableFuture<GuessResponse> {
-        TODO("Not yet implemented")
+    ): CompletableFuture<GuessResponse> =
+        scope.future { handleResponse(api.getGuess(gameId, guessId)) }
+
+    private fun <T> handleResponse(response: Response<T>): T =
+        if (response.isSuccessful) {
+            response.body() ?: throw RuntimeException()
+        } else {
+            handleUnsuccessfulResponse(response)
+        }
+
+    private fun handleResponse(response: Response<Unit>): Void? =
+        if (response.isSuccessful) {
+            null
+        } else {
+            handleUnsuccessfulResponse(response)
+        }
+
+    private fun <T> handleUnsuccessfulResponse(response: Response<T>): Nothing {
+        val adapter = moshi.adapter(ErrorResponse::class.java)
+        val errorText = response.errorBody().toString()
+        throw adapter.fromJson(errorText)?.let { ApiException(it) } ?: RuntimeException()
     }
 
-    private fun loadProperties(): Properties {
-        val properties = Properties()
-        return CodebreakerServiceImpl::class.java
-            .classLoader
-            .getResourceAsStream(PROPERTIES_FILE)
-            .use {
-                properties.load(it)
-                properties
-            }
-    }
 
 }
 
 private const val PROPERTIES_FILE = "web-service.properties"
 private const val LOG_LEVEL_KEY = "logLevel"
-private const val BASE_URL = "baseUrl"
+private const val BASE_URL_KEY = "baseUrl"
+
+private fun loadProperties(): Properties {
+    val properties = Properties()
+    return CodebreakerServiceImpl::class.java
+        .classLoader
+        .getResourceAsStream(PROPERTIES_FILE)
+        .use {
+            properties.load(it)
+            properties
+        }
+}
+
+private fun buildMoshi(): Moshi =
+    Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .add(OffsetDateTimeAdapter)
+        .build()
+
+private fun buildClient(properties: Properties): OkHttpClient {
+    val interceptor = HttpLoggingInterceptor()
+        .setLevel(
+            HttpLoggingInterceptor.Level.valueOf(
+                properties.getProperty(LOG_LEVEL_KEY).uppercase()
+            )
+        )
+    return OkHttpClient.Builder()
+        .addNetworkInterceptor(interceptor)
+        .build()
+}
+
+private fun buildApi(properties: Properties, moshi: Moshi, client: OkHttpClient): CodebreakerApi =
+    Retrofit.Builder()
+        .baseUrl(properties[BASE_URL_KEY] as String)
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .client(client)
+        .build()
+        .create(CodebreakerApi::class.java)
+
+internal object OffsetDateTimeAdapter {
+
+    @ToJson
+    fun toJson(value: OffsetDateTime?): String? =
+        value?.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+
+    @FromJson
+    fun fromJson(value: String?): OffsetDateTime? =
+        value?.let {
+            OffsetDateTime.parse(it, DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+        }
+}
+
+
---
+++ client/src/test/kotlin/edu/cnm/deepdive/codebreaker/client/service/CodebreakerServiceTest.kt
@@ -0,0 +1,47 @@
+package edu.cnm.deepdive.codebreaker.client.service
+
+import edu.cnm.deepdive.codebreaker.client.dto.GameRequest
+import org.junit.jupiter.api.Assertions.assertAll
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class CodebreakerServiceTest {
+
+    private val service: CodebreakerService = CodebreakerService.instance
+
+    @Test
+    fun startGame() {
+        val response = service
+            .startGame(GameRequest("ABCDEF", 3))
+            .get() // Wait for future to complete.
+        assertAll(
+            { assertEquals(3, response.length) },
+            { assertEquals("ABCDEF", response.pool) },
+            { assertFalse(response.solved) },
+            { assertTrue(response.guesses.isEmpty()) },
+        )
+    }
+
+    @Test
+    fun getGame() {
+    }
+
+    @Test
+    fun deleteGame() {
+    }
+
+    @Test
+    fun submitGuess() {
+    }
+
+    @Test
+    fun getGuess() {
+    }
+
+    @Test
+    fun getInstance() {
+    }
+
+}
