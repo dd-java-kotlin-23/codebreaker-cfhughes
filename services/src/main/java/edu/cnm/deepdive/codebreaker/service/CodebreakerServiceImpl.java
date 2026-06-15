5ddb19dbd57565b122bb5c220c28f8d3fca651f8
nick-bennett <nbennett10@cnm.edu>
2026-06-15 12:24:54 -0600

    Streamlined validation; add utility methods for Game & Guess objects.

--- services/src/main/java/edu/cnm/deepdive/codebreaker/service/CodebreakerServiceImpl.java
+++ services/src/main/java/edu/cnm/deepdive/codebreaker/service/CodebreakerServiceImpl.java
@@ -6,11 +6,11 @@ import edu.cnm.deepdive.codebreaker.client.dto.GuessResponse;
 import edu.cnm.deepdive.codebreaker.client.service.CodebreakerProxy;
 import edu.cnm.deepdive.codebreaker.model.Game;
 import edu.cnm.deepdive.codebreaker.model.Guess;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.function.Function;
-import java.util.function.IntPredicate;
 import java.util.stream.Collectors;
+import org.jetbrains.annotations.NotNull;
 
 class CodebreakerServiceImpl implements CodebreakerService {
 
@@ -21,37 +21,21 @@ class CodebreakerServiceImpl implements CodebreakerService {
 
   @Override
   public CompletableFuture<Game> startGame(String pool, int length) {
-    validateGameConfiguration(pool, length);
-    GameRequest request = new GameRequest(pool, length);
-    return proxy.startGame(request)
-        .thenApply(new Function<GameResponse, Game>() {
-          @Override
-          public Game apply(GameResponse response) {
-            List<Guess> guesses = response
-                .getGuesses()
-                .stream()
-                .map(new Function<GuessResponse, Guess>() {
-                  @Override
-                  public Guess apply(GuessResponse response) {
-                    return new Guess(response.getText(), response.getExactMatches(),
-                        response.getNearMatches(), response.getSolution(), response.getCreated());
-                  }
-                })
-                .collect(Collectors.toList());
-            return new Game(response.getId(), response.getPool(), response.getLength(), guesses, response.getCreated());
-          }
-        });
+    return isGameConfigurationValid(pool, length)
+        ? proxy.startGame(new GameRequest(pool, length))
+            .thenApply(CodebreakerServiceImpl::buildGame)
+        : CompletableFuture.failedFuture(new IllegalArgumentException());
   }
-
 
   @Override
   public CompletableFuture<Game> getGame(String id) {
-    return null;
+    return proxy.getGame(id)
+        .thenApply(CodebreakerServiceImpl::buildGame);
   }
 
   @Override
   public CompletableFuture<Void> deleteGame(String id) {
-    return null;
+    return proxy.deleteGame(id);
   }
 
   @Override
@@ -64,22 +48,31 @@ class CodebreakerServiceImpl implements CodebreakerService {
     // TODO: 6/15/26 Invoke as-yet-unimoplemented shutdown method in proxy.
   }
 
-  private static void validateGameConfiguration(String pool, int length) {
+  private static boolean isGameConfigurationValid(String pool, int length) {
     boolean isValidPool = pool
         .codePoints()
-        .distinct()
-        .noneMatch(new IntPredicate() {
-          @Override
-          public boolean test(int codePoint) {
-            return Character.isWhitespace(codePoint)
-                || Character.isISOControl(codePoint)
-                || !Character.isDefined(codePoint);
-          }
-        });
+        .noneMatch((codePoint) -> Character.isWhitespace(codePoint)
+            || Character.isISOControl(codePoint)
+            || !Character.isDefined(codePoint));
     boolean isValidLength = (length >= MIN_LENGTH && length <= MAX_LENGTH);
-    if (!isValidPool || !isValidLength) {
-      throw new IllegalArgumentException(); // TODO: 6/15/26 Throw InvalidGameConfigurationException
-    }
+    return isValidLength && isValidPool;
+  }
+
+  @NotNull
+  private static Game buildGame(GameResponse response) {
+    List<Guess> guesses = response
+        .getGuesses()
+        .stream()
+        .map(CodebreakerServiceImpl::buildGuess)
+        .collect(Collectors.toCollection(ArrayList::new));
+    return new Game(response.getId(), response.getPool(), response.getLength(), guesses,
+        response.getCreated());
+  }
+
+  @NotNull
+  private static Guess buildGuess(GuessResponse response) {
+    return new Guess(response.getText(), response.getExactMatches(),
+        response.getNearMatches(), response.getSolution(), response.getCreated());
   }
 
 }
